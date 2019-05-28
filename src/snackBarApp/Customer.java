d1bd6d636e5a299920b406634c7cff7e8a727487
Krisli Dimo <krislidimo@gmail.com>
2019-05-28 16:04:18 -0500

    added Customer.java

---
+++ src/snackBarApp/Customer.java
@@ -0,0 +1,29 @@
+package snackBarApp;
+
+public class Customer {
+
+	// variables
+	private int maxId = 0;
+	private int id;
+	private String name;
+	private double cash;
+
+	public Custome(String name, double cash) {
+		this.id = maxId++;
+		this.name = name;
+		this.cash = cash;
+	}
+
+	// getters
+	public String getName() { return name; }
+
+	public double getCash() { return cash; }
+
+	// setters
+	public void setName(String name) { this.name = name; }
+
+	//other methods
+	public void addCash(double newCash) { this.cash += newCash; }
+
+	
+}	
