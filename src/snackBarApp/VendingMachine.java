73adac7bcc8fc27eabdc0c42baa11125c4329d64
Krisli Dimo <krislidimo@gmail.com>
2019-05-28 16:03:55 -0500

    added VendingMachine.Java

---
+++ src/snackBarApp/VendingMachine.java
@@ -0,0 +1,24 @@
+package snackBarApp;
+
+public class VendingMachine {
+
+	// variables
+	private int maxId = 0;
+	private int id;
+	private String name;
+
+	public VendingMachine(String name) {
+		this.id = maxId++;
+		this.name = name;
+	}
+
+	// getters
+	public int getId() { return id; }
+
+	public String getName() { return name; }
+
+	// setters
+	public void setId(int id) { this.id = id}
+
+	public void setName(String name) { this.name = name}
+}
