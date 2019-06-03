a375ad361fc19cfe7dcc2714863f8e8abcf5358e
Sydney Crumley <srsimps19@gmail.com>
2019-06-03 14:53:27 -0500

    built out snack fields, and getters and setters

---
+++ SnackBarAPP/src/snackbarApp/Snack.java
@@ -0,0 +1,69 @@
+package snackbarApp;
+
+public class Snack
+{
+    //fields
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private int quanitity;
+    private double cost;
+    private int vendingID;
+
+    public Snack(String name, int quanitity, double cost, int vendingID)
+    {
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.quanitity = quanitity;
+        this.cost = cost;
+        this.vendingID = vendingID;
+    }
+
+    //getters and setters
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public int getQuanitity()
+    {
+        return quanitity;
+    }
+
+    public void setQuanitity(int quanitity)
+    {
+        this.quanitity = quanitity;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
+    public int getVendingID()
+    {
+        return vendingID;
+    }
+
+    public void setVendingID()
+    {
+        this.vendingID = vendingID;
+    }
+}
