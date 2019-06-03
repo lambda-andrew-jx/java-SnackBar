480aa9beda300bdb8f7a9db6edebf15b5b073e40
atolmie <atolmie22@gmail.com>
2019-06-03 14:58:08 -0500

    first commit

---
+++ Customers.java
@@ -0,0 +1,14 @@
+package JAVA-SNACKBAR;
+
+public class Customers
+{
+   private int id;
+   private String name;
+   private double cashonhand; 
+
+}
+
+//Customer has id, name, cash on hand. 
+//Customer can add cash, buy given total cash used in purchase, get and set name, get cash on hand.  
+//* Jane with $45.25
+//* Bob with $33.14
